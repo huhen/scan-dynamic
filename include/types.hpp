255c23ab24249b2501ec2fe9f3a5911ad7415375
huhen <kinetic@bk.ru>
2025-06-16 15:45:17 +0000

    Add get method

--- include/types.hpp
+++ include/types.hpp
@@ -16,6 +16,11 @@ template <typename... Ts>
 struct scan_result {
     std::tuple<Ts...> scan_values;
     const std::tuple<Ts...> &values(void) { return scan_values; }
+
+    template <std::size_t Idx>
+    constexpr const auto &get() {
+        return std::get<Idx>(scan_values);
+    }
 };
 
 }  // namespace stdx::details
